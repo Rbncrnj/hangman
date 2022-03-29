437f5c8df7cba31688e7d263bc06628f62e19e61
ruben <rbncrnj707@gmail.com>
2022-03-28 20:05:33 -0700

    set up gamegrid

---
+++ Hangman.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Hangman {
+    public static void main (String[] args){
+        String codeword = "blind";
+        ArrayList<Character> guesses = new ArrayList<Character>();
+        int lives = 6;
+        displayGameGrid(codeword, guesses, lives);
+
+    }
+    public static void displayGameGrid(String codeWord, ArrayList<Character> guesses, int lives){
+        System.out.println("this is the grid");
+        System.out.println("This is the codeword " + codeWord);
+    }
+}
