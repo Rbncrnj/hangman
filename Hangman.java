64d4b375912c369d954617444632482cf9010b04
ruben <rbncrnj707@gmail.com>
2022-04-21 09:51:16 -0700

    made visual of hangman with println

--- Hangman.java
+++ Hangman.java
@@ -1,15 +1,22 @@
 import java.util.ArrayList;
 
 public class Hangman {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         String codeword = "blind";
         ArrayList<Character> guesses = new ArrayList<Character>();
         int lives = 6;
         displayGameGrid(codeword, guesses, lives);
 
     }
-    public static void displayGameGrid(String codeWord, ArrayList<Character> guesses, int lives){
+
+    public static void displayGameGrid(String codeWord, ArrayList<Character> guesses, int lives) {
         System.out.println("this is the grid");
         System.out.println("This is the codeword " + codeWord);
+        System.out.println("______");
+        System.out.println("|     O");
+        System.out.println("|   --|--");
+        System.out.println("|    / \\");
+        System.out.println("______");
+        System.out.println("_ _ _ _ _");
     }
 }
